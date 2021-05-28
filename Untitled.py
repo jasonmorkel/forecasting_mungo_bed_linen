8d6d57cba154f2990b130bd78fff292768cf245a
Jason Morkel <jkmorkel@gmail.com>
2021-05-28 11:48:47 +0200

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+
+# In[24]:
+
+
+df = pd.read_excel (r'C:\Users\Jay\Documents\Data & exercises\Forecasting ARIMA (Mungo)\mungo.xlsx')
+bed_linen = df.loc[df['Category'] == 'Bed Linen']
+
+
+# In[25]:
+
+
+bed_linen['Order Date'].min(), bed_linen['Order Date'].max()
+
+
+# In[26]:
+
+
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
+bed_linen.drop(cols, axis=1, inplace=True)
+bed_linen = bed_linen.sort_values('Order Date')
+bed_linen.isnull().sum()
+
+
+# In[27]:
+
+
+bed_linen = bed_linen.groupby('Order Date')['Sales'].sum().reset_index()
+
+
+# In[28]:
+
+
+bed_linen = bed_linen.set_index('Order Date')
+bed_linen.index
+
+
+# In[29]:
+
+
+y = bed_linen['Sales'].resample('MS').mean()
+
+
+# In[30]:
+
+
+y['2017':]
+
+
+# In[31]:
+
+
+y.plot(figsize=(15, 6))
+plt.show()
+
+
+# In[32]:
+
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
+
+
+# In[33]:
+
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+
+# In[42]:
+
+
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 0, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+results = mod.fit()
+print(results.summary().tables[1])
+
+
+# In[47]:
+
+
+pred = results.get_prediction(start=pd.to_datetime('2017-01-01'), dynamic=False)
+pred_ci = pred.conf_int()
+ax = y['2014':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+ax.set_xlabel('Date')
+ax.set_ylabel('Bed Linen Sales')
+plt.legend()
+plt.show()
+
+
+# In[48]:
+
+
+pred_uc = results.get_forecast(steps=100)
+pred_ci = pred_uc.conf_int()
+ax = y.plot(label='observed', figsize=(14, 7))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Bed Linen Sales')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
